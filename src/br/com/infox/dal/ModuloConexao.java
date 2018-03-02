18beabfe9eaef1e0277c992e89ace9e96cb8e34c
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-03-01 22:12:00 -0300

    iniciando Projeto Java com Mysql

---
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,26 @@
+package br.com.infox.dal;
+
+import java.sql.*;
+
+public class ModuloConexao {
+
+    //método por responsável por estabelecer a conexão com o BD
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/dbinfox";
+        String user = "root";
+        String password = "";
+
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+
+    }
+
+}
